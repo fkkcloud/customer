b91d059162b220614f8d1f6f16f537688bcb9a0d
Jae <fkkcloud@gmail.com>
2016-03-17 01:24:27 -0700

    ionic list in + phaser game in

--- cuz/js/controllers.js
+++ cuz/js/controllers.js
@@ -1,7 +1,18 @@
 angular.module('game.controllers', [])
 
 .controller('DashCtrl', function($scope, $state, $timeout, GameServerSvc) {
-
+  $scope.items = [
+    { item: 'haha' },
+    { item: 'haha2' },
+    { item: 'haha3' },
+    { item: 'haha4' },
+    { item: 'haha5' },
+    { item: 'haha6' },
+    { item: 'haha7' },
+    { item: 'haha8' },
+    { item: 'haha9' },
+    { item: 'haha10' },
+  ]
 })
 
 .controller('AboutCtrl', function($scope) {
